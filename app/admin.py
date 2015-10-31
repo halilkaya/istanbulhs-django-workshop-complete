fc7748a18f5e1a5ffb14cf8c638f7d70ded1d848
Halil Kaya <kayahalil@gmail.com>
2015-10-31 16:53:36 +0200

    Unicode problem solved and slug fields slugified

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,13 @@
+# coding: utf-8
+from __future__ import unicode_literals
 from django.contrib import admin
 from app.models import Category, Post
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    prepopulated_fields = {'slug': ('name',)}
 
 class PostAdmin(admin.ModelAdmin):
-    pass
+    prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib import auth
 
@@ -8,14 +9,14 @@ class Category(models.Model):
     Category Model
     """
     slug = models.SlugField(unique=True, blank=False, verbose_name='Slug')
-    parent = models.ForeignKey('Category', related_name='child', verbose_name='Üst Kategori')
+    parent = models.ForeignKey('Category', related_name='child', verbose_name='Üst Kategori', blank=True, null=True)
     name = models.CharField(max_length=100, blank=False, verbose_name='Kategori')
 
     class Meta:
         verbose_name = 'Kategori'
         verbose_name_plural = 'Kategoriler'
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
 
 
@@ -26,10 +27,10 @@ class Post(models.Model):
     slug = models.SlugField(unique=True, blank=False, verbose_name='Slug')
     title = models.CharField(max_length=255, blank=False, verbose_name='Başlık')
     author = models.ForeignKey('auth.User', related_name='author', blank=False, verbose_name='Yazar')
-    thumbnail = models.ImageField(verbose_name='Resim')
-    content = models.TextField(verbose_name='İçerik')
+    thumbnail = models.ImageField(verbose_name='Resim', blank=True)
+    content = models.TextField(verbose_name='İçerik', blank=True)
     category = models.ForeignKey('Category', related_name='category', blank=False, verbose_name='Kategori')
-    tags = models.CharField(max_length=255, verbose_name='Etiket(ler)')
+    tags = models.CharField(max_length=255, verbose_name='Etiket(ler)', blank=True)
     is_page = models.BooleanField(default=False, verbose_name='Bu bir sayfa mı?')
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
@@ -38,5 +39,5 @@ class Post(models.Model):
         verbose_name = 'İçerik'
         verbose_name_plural = 'İçerikler'
 
-    def __str__(self):
+    def __unicode__(self):
         return self.title
