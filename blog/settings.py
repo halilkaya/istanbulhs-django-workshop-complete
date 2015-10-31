b5ad6b15e36a990b5d13780e6d1786a45ddec2f5
Adil ÖZTAŞER <adiloztaser@gmail.com>
2015-10-31 21:53:38 +0300

    page list added on header

--- blog/settings.py
+++ blog/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'app.context_processor.category_list',
+                'app.context_processor.page_list',
             ],
         },
     },
--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from app import views
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^(?P<post_slug>[-_\w]+)/$', views.post_single, name='post_single'),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
 ]
